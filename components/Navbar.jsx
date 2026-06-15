4141c895fef58fb95ae7a753b3a577c1e7fbb329
XAct1HP <trickylion05@gmail.com>
2026-06-15 17:48:05 -0400

    Navbar stretch

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar() {
 
   return (
     <nav className="w-full bg-navy">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="px-4 sm:px-6 lg:px-10">
         <div className="flex items-center justify-between h-16">
           {/* Logo / Home */}
           <div className="flex-shrink-0">
